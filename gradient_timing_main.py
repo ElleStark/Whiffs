3d3a0730a168ed57cb1f90a53d32b4447940be33
ElleStark <elle.stark5645@gmail.com>
2025-01-13 14:36:16 -0700

    initial relative timing & correlation calcs

--- gradient_timing_main.py
+++ gradient_timing_main.py
@@ -1,7 +1,7 @@
 # Script to compute the relative timing of flow cue and odor GRADIENT ridges
 # Elle Stark January 2025
 
-import datafield
+from src import datafield 
 import h5py
 import logging
 import matplotlib.pyplot as plt
@@ -20,13 +20,12 @@ def main():
     # load data, subset data by index as needed
     integration_T_idx = 62
     
-    xmin = 399
-    xmax = 401
+    xmin = 1449
+    xmax = 1451
     ymin = 99
     ymax = 101
-    tmin = integration_T_idx + 5000
-    tmax = tmin + 500
-    time = 800
+    tmin = integration_T_idx 
+    tmax = 9000
 
     xrange = slice(xmin, xmax)
     yrange = slice(ymin, ymax)
@@ -56,54 +55,61 @@ def main():
         ftle_data = f2.get('FTLE_back_1_25s_finegrid')[tmin-integration_T_idx:tmax-integration_T_idx, ymin*2:ymax*2, xmin*2:xmax*2]
         # strain = f.get('maxPstrain')[time, xlims, ylims]
 
-    # define flow cue 
-    FTLE_x = np.linspace(x_grid[0, 0], x_grid[0, -1], ftle_data.shape[0]-1)  # FTLE grid finer than odor grid
-    FTLE_y = np.linspace(y_grid[0, 0], y_grid[-1, 0], ftle_data.shape[1]-1)  # FTLE grid finer than odor grid
+    # define flow cue grid if needed for FTLE data
+    FTLE_x = np.linspace(x_grid[0, 0], x_grid[-1, 0], ftle_data.shape[1]-1)  # FTLE grid finer than odor grid
+    FTLE_y = np.linspace(y_grid[0, 0], y_grid[0, -1], ftle_data.shape[2]-1)  # FTLE grid finer than odor grid
     FTLE_x, FTLE_y = np.meshgrid(FTLE_x, FTLE_y)
 
-
-    ftle_cgrad = datafield.DataField(ftle_data, FTLE_x, FTLE_y, dt)
-
-    # define odor cue
+    # create instance of class DataField for computations
+    ftle_cgrad = datafield.DataField(odor_gradient, x_grid, y_grid, dt, dx, ftle_data, FTLE_x, FTLE_y, dt, round(FTLE_x[0, 1]-FTLE_x[0, 0], 5))
+    DEBUG(f'fdx: {ftle_cgrad.fdx}')
 
     # QC plot: time series of flow & odor data at select points
-    DEBUG(f'odor gradient dims: {odor_gradient.shape}')
-    testpt_x = 1
-    testpt_y = 1
-    flow_ts = flow_data[:, testpt_y*2, testpt_x*2]
-    flow_ts = (flow_ts - np.min(flow_ts)) / (np.max(flow_ts)-np.min(flow_ts))
-    odor_ts = odor_gradient[:, testpt_y, testpt_x]
-    odor_ts = (odor_ts - np.min(odor_ts)) / (np.max(odor_ts) - np.min(odor_ts))
-    
-    # Plot each time series
-    fig, ax = plt.subplots(figsize=(10, 4))
-    plt.plot(time_vec, flow_ts, label='FTLE')
-    plt.plot(time_vec, odor_ts, label='odor gradient')
-
-    # Plotting parameters
-    plt.title(f'FTLE and odor gradients, [x, y] = [{round((xmin+1)*dx, 2)}, {round(0.3-(ymin+1)*dx, 2)}], t={round(tmin*dt, 1)} to {round(tmax*dt, 1)} s')
-    plt.xlabel('time (s)')
-    plt.ylabel('normalized value')
-    plt.legend()
-    plt.savefig(f'ignore/plots/c_grad_ts/FTLEodorgrad_x{round((xmin+1)*dx, 2)}_y{round(0.3-(ymin+1)*dx, 2)}]_t{round(tmin*dt, 1)}to{round(tmax*dt, 1)}s.png', dpi=300)
-    plt.show()
- 
-    # define flow cue 
-
-
-    # define odor cue
-
+    # ftle_cgrad.plot_time_series(time_vec, 1, 1, xmin, ymin)
 
     # Find odor cue ridge indexes for each location
-    
-
-    # Select local window for each ridge
-    w_dur = 0.50  # duration in sec
+    compute_pts = [(1, 1)]  # list of tuples for each point to be analyzed
+    odor_threshold = 10E-5
+    # required window size for each ridge
+    w_dur = 1  # duration in sec
     w_idx_dur = w_dur/dt
+    for pt in compute_pts:
+        odor_ridges = ftle_cgrad.find_odor_ridges(odor_threshold, pt[1], pt[0], distance=int(w_idx_dur/2))
 
-    # Find timing of local max flow cue in each window
+        # QC plot: time series of flow & odor data at select points with ridges
+        # ftle_cgrad.plot_time_series(time_vec, 1, 1, xmin, ymin, ridges=odor_ridges[0], save=True)
 
+        # Find timing of local max flow cue peaks in each window
+        flow_peaks, corrs = ftle_cgrad.find_loc_max_fcue(pt[1], pt[0], odor_ridges[0], w_idx_dur, corr=True, yidx=pt[1], xidx=pt[0])
+        valid_fpeaks = flow_peaks[flow_peaks>-100]
+        valid_corrs = corrs[corrs>-100]
+        INFO(f'Number of gradient ridges with corresponding flow cue peaks: {len(valid_fpeaks)}')
+        INFO(f'average correlation: {np.mean(valid_corrs)}')
 
+        # Box plot of relative flow peak timing
+        # plt.boxplot(valid_fpeaks)
+        # plt.ylim=(-w_idx_dur/2, w_idx_dur/2)
+        # plt.show()
+
+        # Histogram of relative flow peak timing
+        plt.hist(valid_fpeaks, bins=15)
+        plt.title(f'relative FTLE timing, x{round((xmin+1)*dx, 2)} y{0.3-round((ymin+1)*dx)} t=1.25 to 180 s, window={w_dur} s, n_peaks={len(valid_fpeaks)}')
+        plt.savefig(f'ignore/plots/c_grad_ts/FTLEodorgrad_hist_x{round((xmin+1)*dx, 2)}_y{round((ymin+1)*dx)}]_\
+                t{round(np.min(time_vec), 1)}to{round(np.max(time_vec), 1)}s_wdur{w_dur}s.png', dpi=300)
+        plt.show()
+
+        # Boxplot of correlation values w/ mean & std dev labeled
+        fig, ax = plt.subplots(figsize=(6, 6))
+        ax.boxplot(valid_corrs)
+        plt.title(f'pearson correlations, avg {round(np.mean(valid_corrs), 2)}, std {round(np.std(valid_corrs), 2)}, x{round((xmin+1)*dx, 2)} y{round(0.3-(ymin+1)*dx, 2)} t=1.25 to 180 s, window={w_dur}s')
+        # ax.text(1.5, 2.5, f'mean: {np.mean(valid_corrs)}', fontsize=12)
+        # ax.text(1.5, 2.5, 'test', fontsize=12)
+        #ax.text(2, 2.5, f'std dev: {np.std(valid_corrs)}', fontsize=12)
+        plt.savefig(f'ignore/plots/c_grad_ts/FTLEodorgrad_corrs_x{round((xmin+1)*dx, 2)}_y{round(0.3-(ymin+1)*dx, 2)}]_\
+                        t{round(np.min(time_vec), 1)}to{round(np.max(time_vec), 1)}s.png', dpi=300)
+        plt.show()
+    
+    
     # Display distribution of timing for location, if desired
 
 
--- src/datafield.py
+++ src/datafield.py
@@ -5,6 +5,7 @@ For use in computing relative timing of flow and odor cue ridges
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as signal
 
 logger = logging.getLogger('GradientTiming')
 logger.setLevel(logging.DEBUG)
@@ -44,7 +45,7 @@ class DataField:
         self.fdx = flow_dx
         
 
-    def plot_time_series(self, time_vec, o_xidx, o_yidx, o_xloc, o_yloc):
+    def plot_time_series(self, time_vec, o_xidx, o_yidx, o_xloc, o_yloc, save=False, ridges=None, ridges2=None):
         """_summary_
 
         Args:
@@ -53,6 +54,7 @@ class DataField:
             yidx (int): y index within the existing data arrays to use for selecting point to plot
             xloc (int): x index relative to the entire domain, to identify location in title and file name
             yloc (int): y index relative to the entire domain, to identify location in title and file name
+            ridges (1d array): array of times for plotting vertical lines at ridge locations
         """
 
         # QC plot: time series of flow & odor data at select points
@@ -69,10 +71,92 @@ class DataField:
         plt.plot(time_vec, flow_ts, label='FTLE')
         plt.plot(time_vec, odor_ts, label='odor gradient')
 
+        # Plot vertical lines at ridge locations, if any
+        if ridges is not None:
+            ridge_times = time_vec[0] + ridges*self.odt
+            plt.vlines(ridge_times, ymin=0, ymax=1, color='red', linestyle='dashed')
+
+        if ridges2 is not None:
+            ridge2_times = time_vec[0] + ridges*self.odt
+            plt.vlines(ridge2_times, ymin=0, ymax=1, color='blue', linestyle='dashed')
+
         # Plotting parameters
-        plt.title(f'FTLE and odor gradients, [x, y] = [{round((o_xloc+1)*self.odx, 2)}, {round(max(self.oy[:, 0])-(o_yloc+1)*self.odx, 2)}], t={round(np.min(time_vec)*self.odt, 1)} to {round(np.max(time_vec)*self.odt, 1)} s')
+        plt.title(f'FTLE and odor gradients, [x, y] = [{round((o_xloc+1)*self.odx, 2)}, {round(max(self.oy[:, 0])-(o_yloc+1)*self.odx, 2)}]\
+                  t={round(np.min(time_vec), 1)} to {round(np.max(time_vec), 1)} s')
         plt.xlabel('time (s)')
         plt.ylabel('normalized value')
         plt.legend()
-        plt.savefig(f'ignore/plots/c_grad_ts/FTLEodorgrad_x{round((o_xloc+1)*self.odx, 2)}_y{round(max(self.oy[:, 0])-(o_yloc+1)*self.odx, 2)}]_t{round(np.min(time_vec)*self.odt, 1)}to{round(np.max(time_vec)*self.odt, 1)}s.png', dpi=300)
+        if save:
+            plt.savefig(f'ignore/plots/c_grad_ts/FTLEodorgrad_x{round((o_xloc+1)*self.odx, 2)}_y{round(max(self.oy[:, 0])-(o_yloc+1)*self.odx, 2)}]_\
+                        t{round(np.min(time_vec), 1)}to{round(np.max(time_vec), 1)}s.png', dpi=300)
         plt.show()
+
+    def find_odor_ridges(self, o_thrs, yidx, xidx, timelims=slice(None, None), distance=None, width=None):
+        """_summary_
+
+        Args:
+            o_thrs (_type_): _description_
+            yidx (_type_): _description_
+            xidx (_type_): _description_
+            timelims (_type_, optional): _description_. Defaults to slice(None, None).
+            distance (_type_, optional): _description_. Defaults to None.
+            width (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        # select time series of odor data for finding ridge indexes
+        odor_ts = self.odata[timelims, yidx, xidx]
+
+        # identify peaks using scipy signal processing package
+        ridge_idxs = signal.find_peaks(odor_ts, height=o_thrs, distance=distance, width=width)
+
+        # return list of indexes with odor ridge times
+        return ridge_idxs
+    
+    def find_loc_max_fcue(self, f_yidx, f_xidx, w_ctrs, w_dur_idx, timelims=slice(None, None), corr=False, yidx=None, xidx=None):
+
+        # select time series of flow cue data for finding max local peaks
+        flow_ts = self.fdata[timelims, f_yidx, f_xidx]
+        flow_peaks = np.empty_like(w_ctrs)
+        flow_peaks[:] = -999
+
+        if corr:
+            odor_ts = self.odata[timelims, yidx, xidx]
+            corrs = np.empty(len(w_ctrs))
+            corrs[:] = -999
+        else:
+            corrs = None
+
+        i=0
+        for ctr in w_ctrs:
+            f_ts = flow_ts[ctr-int(w_dur_idx/2):ctr+int(w_dur_idx/2)]
+            peak = signal.find_peaks(f_ts, distance=w_dur_idx)[0]
+            if len(peak) == 0:
+                flow_peaks[i] = -999
+            elif len(peak) == 1:
+                flow_peaks[i] = peak - int(w_dur_idx/2)
+            else:
+                INFO('Too many peaks found, check window duration')
+            
+            # QC: line plot of flow time series for each window with peak overlaid
+            # plt.plot(f_ts)
+            # plt.vlines(peak, 0, 1)
+            # plt.show()
+
+            if (corr & len(peak)) == 1:
+                o_ts = odor_ts[ctr-int(w_dur_idx/2):ctr+int(w_dur_idx/2)]
+                o_ts = (o_ts-min(o_ts)) / (max(o_ts)-min(o_ts))
+                w_corr = np.corrcoef(o_ts, f_ts)[0, 1]
+                corrs[i] = w_corr
+
+                # QC: plot of odor and flow in window
+                # plt.plot(o_ts, label='odor gradient')
+                # plt.plot(f_ts, label='flow')
+                # plt.legend()
+                # plt.show()
+
+            i+=1
+
+        return flow_peaks, corrs
+
